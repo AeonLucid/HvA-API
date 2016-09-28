729a95e2d31da0dd5638f6c0be25696cda08763e
AeonLucid <aeonlucid@outlook.com>
2016-09-28 19:34:52 +0200

    Testing out deployment.

---
+++ src/HvA-API/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HvA_API.Controllers
+{
+    [Route("[controller]")]
+    public class AboutController : Controller
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
