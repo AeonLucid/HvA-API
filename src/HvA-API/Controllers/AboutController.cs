536f9518c2580986e1cfb3282740cd98f6aa7be3
AeonLucid <aeonlucid@outlook.com>
2016-09-29 02:25:53 +0200

    Update `HvA.API.NetStandard1` to 1.1.0 to have reauthentication.

--- src/HvA-API/Controllers/AboutController.cs
+++ src/HvA-API/Controllers/AboutController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HvA_API.Controllers
 {
     [Route("[controller]")]
