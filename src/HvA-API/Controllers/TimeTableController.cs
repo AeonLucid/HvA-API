1e6a1d5c2a0df66104722ead287b879e18d14a8f
AeonLucid <aeonlucid@outlook.com>
2016-10-26 14:23:19 +0200

    Add timetables endpoint

---
+++ src/HvA-API/Controllers/TimeTableController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using HvA.API.NetStandard1;
+using HvA.API.NetStandard1.Data;
+using HvA_API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace HvA_API.Controllers
+{
+    [Route("[controller]")]
+    public class TimeTableController : Controller
+    {
+        private readonly HvAClient _client;
+        private readonly IMemoryCache _memoryCache;
+
+        public TimeTableController(HvAClient client, IMemoryCache memoryCache)
+        {
+            _client = client;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        [Route("{studentId}/{weekNumber:int}")]
+        public async Task<TimetableItem[]> GetTimeTableWeek(string studentId, int weekNumber)
+        {
+            var cacheKey = $"TimeTableController-GetTimeTableWeek-{weekNumber}";
+            
+            return await _memoryCache.GetValueAsync(cacheKey, async () => await _client.GetOtherTimeTableAsync(studentId, weekNumber), TimeSpan.FromMinutes(5));
+        }
+    }
+}
